6d1fbee5627cff0b02909601d9294bb92ba6bb6c
Caro <55109108+CaroMen@users.noreply.github.com>
2021-06-04 15:18:03 -0500

    array problems started

--- react-app/src/components/Dashboard/Arrays/FullArraysProblems.js
+++ react-app/src/components/Dashboard/Arrays/FullArraysProblems.js
@@ -49,39 +49,52 @@ const ArrayProblems = () => {
             </div>
             <div className="problems-div">
                 {problems.map((problem) => (
-                    <div>
-                        <div>{problem.title}</div>
-                        <div>{problem.category}</div>
-                        <div>{problem.description}</div>
-                        <SyntaxHighlighter
-                            language="python"
-                            wrapLines={true}
-                            style={duotoneLight}
-                        >
-                            {problem.examples}
-                        </SyntaxHighlighter>
-                        <SyntaxHighlighter
-                            language="python"
-                            lineProps={{ style: { wordBreak: 'break-all', whiteSpace: 'pre-wrap' } }}
-                            style={materialOceanic}
-                            showLineNumbers={true}
-                            wrapLines={true}>
-                            {problem.solution}
-                        </SyntaxHighlighter>
-                        <div>
-                            <h3>Solved</h3>
-                            <input type="radio" name="checked" onChange={() => setSolved(true)} />
-                            <button disabled={!solved} onClick={addProblem}>Save Changes</button>
+                    <div className="problem-div-container">
+                        <div className="prob-title">Question: {problem.title}</div>
+                        <div className="prob-cat">Category: {problem.category}</div>
+                        <div className="prob-desc">{problem.description}</div>
+                        <div className="code-blocks">
+                            <div className="examples">
+                                Example:
+                                <SyntaxHighlighter
+                                    language="python"
+                                    wrapLines={true}
+                                    style={duotoneLight}
+                                >
+                                    {problem.examples}
+                                </SyntaxHighlighter>
+                            </div>
+                            <div className="code-block">
+                                <SyntaxHighlighter
+                                    language="python"
+                                    lineProps={{ style: { wordBreak: 'break-all', whiteSpace: 'pre-wrap' } }}
+                                    style={materialOceanic}
+                                    showLineNumbers={true}
+                                    wrapLines={true}>
+                                    {problem.solution}
+                                </SyntaxHighlighter>
+                            </div>
                         </div>
-                        <div>
-                            <h3>Review</h3>
-                            <input type="radio" name="checked" onChange={() => setChoice(true)} />
-                            <button disabled={!choice} onClick={addReview}>Save Changes</button>
+                        <div className="solved-mark">
+                            <label className="pill-btn">
+                                <input className="radio-btn" type="radio" name="checked" onChange={() => setSolved(true)} />
+                                <h3 className="label">Solved</h3>
+                            </label>
+                            <div disabled={!solved} onClick={addProblem}><i class="fas fa-check-square"></i></div>
+                        </div>
+                        <div className="review-mark">
+                            <div className="pill-btn">
+                                <input className="radio-btn" type="radio" name="checked" onChange={() => setChoice(true)}>
+
+                                </input>
+                                <h3 className="label">Review</h3>
+                            </div>
+                            <div disabled={!choice} onClick={addReview}><i class="fas fa-check-square"></i></div>
                         </div>
                     </div>
                 ))}
             </div>
-        </div>
+        </div >
     )
 }
 
