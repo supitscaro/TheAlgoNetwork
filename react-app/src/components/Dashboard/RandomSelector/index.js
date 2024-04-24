4840e6faf4533f356832718c5bbcaa3f3e70f382
thisiscaro <carolinemmen@gmail.com>
2024-04-24 17:46:43 -0500

    remove console logs

--- react-app/src/components/Dashboard/RandomSelector/index.js
+++ react-app/src/components/Dashboard/RandomSelector/index.js
@@ -1,86 +1,84 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 
 import { getEveryProblem } from "../../../store/problems";
 import { allSolved } from "../../../store/solved";
 
-import './random.css';
+import "./random.css";
 
 const RandomProblem = () => {
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const user = useSelector(state => state.session?.user);
-    const allProblems = useSelector(state => state.problems?.allProblems);
-    const problemsSolvedList = useSelector(state => state.solvedLists?.allSolvedLists);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const user = useSelector((state) => state.session?.user);
+  const allProblems = useSelector((state) => state.problems?.allProblems);
+  const problemsSolvedList = useSelector(
+    (state) => state.solvedLists?.allSolvedLists
+  );
 
-    // create a list of the problem's id a user has solved
-    let listOfSolved = []
+  // create a list of the problem's id a user has solved
+  let listOfSolved = [];
 
-    for (let i in problemsSolvedList) {
-        let problem = problemsSolvedList[i];
+  for (let i in problemsSolvedList) {
+    let problem = problemsSolvedList[i];
 
-        if (user?.id === problem?.users_id) {
-            listOfSolved.push(problem.problems_id);
-        }
+    if (user?.id === problem?.users_id) {
+      listOfSolved.push(problem.problems_id);
     }
+  }
 
-    listOfSolved.sort();
-    // create a list of every problem's id to then filter if the id is present in listOfSolved
-    let unsolvedProblems = []
+  listOfSolved.sort();
+  // create a list of every problem's id to then filter if the id is present in listOfSolved
+  let unsolvedProblems = [];
 
-    for (let i in allProblems) {
-        let problem = allProblems[i];
-        unsolvedProblems.push(problem.id);
+  for (let i in allProblems) {
+    let problem = allProblems[i];
+    unsolvedProblems.push(problem.id);
+  }
+
+  // create a new list of all the id's not present in user's solved list
+  let problemsToStudy = [];
+
+  for (let i = 0; i < unsolvedProblems.length; i++) {
+    let id = unsolvedProblems[i];
+    let bool = listOfSolved.includes(id);
+    if (!bool) {
+      problemsToStudy.push(id);
     }
+  }
 
-    console.log('unsolved problems', unsolvedProblems);
+  // randomly get an id of the problems the user has left to study
+  let randomProblemId = Math.floor(Math.random() * problemsToStudy.length);
 
-    // create a new list of all the id's not present in user's solved list
-    let problemsToStudy = [];
+  // grab the category and id to redirect a user to that problem they haven't solved
+  let category;
+  let id;
+  for (let i in allProblems) {
+    let problem = allProblems[i];
 
-    for (let i = 0; i < unsolvedProblems.length; i++) {
-        let id = unsolvedProblems[i];
-        let bool = listOfSolved.includes(id);
-        if (!bool) {
-            problemsToStudy.push(id);
-        }
-    };
-
-    console.log('problems to study', problemsToStudy);
-
-    // randomly get an id of the problems the user has left to study
-    let randomProblemId = Math.floor(Math.random() * problemsToStudy.length);
-
-    // grab the category and id to redirect a user to that problem they haven't solved
-    let category;
-    let id;
-    for (let i in allProblems) {
-        let problem = allProblems[i];
-
-        if (problem.id === problemsToStudy[randomProblemId]) {
-            category = problem.category;
-            id = problem.id;
-        }
+    if (problem.id === problemsToStudy[randomProblemId]) {
+      category = problem.category;
+      id = problem.id;
     }
+  }
 
-    useEffect(() => {
-        dispatch(allSolved())
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(allSolved());
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(getEveryProblem())
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(getEveryProblem());
+  }, [dispatch]);
 
-    const onClick = () => {
-        console.log('button works');
-        history.push(`/${category}/${id}`);
-    }
+  const onClick = () => {
+    history.push(`/${category}/${id}`);
+  };
 
-    return (
-        <div className="rand-btn" onClick={onClick}>Generate a Random Problem</div>
-    )
-
-}
+  return (
+    <div className="rand-btn" onClick={onClick}>
+      Generate a Random Problem
+    </div>
+  );
+};
 
 export default RandomProblem;
